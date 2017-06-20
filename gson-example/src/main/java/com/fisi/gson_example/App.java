1aa9188c40b27cf9108cbd3a18c145cbcb3e21ce
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-06-19 23:05:33 -0500

    Initial

---
+++ gson-example/src/main/java/com/fisi/gson_example/App.java
@@ -0,0 +1,13 @@
+package com.fisi.gson_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
