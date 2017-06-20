62281baa663359ee2e940eabf2be09ac5267dddf
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-06-19 23:14:37 -0500

    Main Class finished

--- gson-example/src/main/java/com/fisi/gson_example/App.java
+++ gson-example/src/main/java/com/fisi/gson_example/App.java
@@ -1,13 +1,42 @@
 package com.fisi.gson_example;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Staff staff = createDummyObject();
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(staff);
+		System.out.println(json);
+	}
+	
+	private static Staff createDummyObject(){
+		
+		Staff staff = new Staff();
+		
+		staff.setName("Alonso");
+		staff.setAge(35);
+		staff.setPosition("Founder");
+		staff.setSalary(new BigDecimal("10000"));
+		
+		List<String> skills = new ArrayList<String>();
+		skills.add("java");
+		skills.add("python");
+		skills.add("shell");
+		
+		staff.setSkills(skills);
+		
+		return staff;
+		
+		
+	}
 }
